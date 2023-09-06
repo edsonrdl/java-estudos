fc8396ec82dde9596a9bea979390ee1bf95a2bc7
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-06 12:07:21 -0300

    feat:Criado condicional if

---
+++ 2. Estrutura de controle/2.1. Condicionais/2.1.1. if/condicionalIf.java
@@ -0,0 +1,10 @@
+public class condicionalIf {
+    public static void main(String[] args){
+        int nota=65;
+        int media=60;
+        if(nota>=media){
+            System.out.println("Maior");
+        }
+        System.out.println("Fim do programa");
+    }
+}
