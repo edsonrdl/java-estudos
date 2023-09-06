e928001f8a00533ab28ac61b611f218d46063542
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-05 23:11:55 -0300

    feat:Criado variáveis

---
+++ 1. Variáveis e tipos de dados/1.1. Variáveis/ex1/variaveis.java
@@ -0,0 +1,8 @@
+package ex1;
+
+public  class variaveis {
+    public static void main(String[] args){
+        int num=100;
+        System.out.printf("O valor da variável é num:%d",num);
+    }
+}
