af8f210ca56565f3a2eb5786cdd0a8ed74427483
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-06 12:09:55 -0300

    feat:Criado condicional if else

---
+++ 2. Estrutura de controle/2.1. Condicionais/2.1.2. else if/condicionalElseIf.java
@@ -0,0 +1,12 @@
+public class condicionalElseIf {
+   public static void main (String[] args){
+    int nota=60;
+    int media=65;
+    if(nota>=media){
+        System.out.println("O aluno foi aprovado");
+    }else{
+         System.out.println("Aluno está em recuperação");
+    }
+    System.out.print("Fim do programa");
+   } 
+}
