6a17acd00acff2b9ba670ac44a53df11d85a5dbb
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-19 18:04:46 -0300

    feat:Criado Exemplo do loop for

---
+++ 2. Estrutura de controle/2.2. loops/2.2.1 for/Ex01/loopForEx01.java
@@ -0,0 +1,8 @@
+public class loopForEx01 {
+    public static void main(String[] args) {
+        for(int cont=0;cont<=10;cont++){
+           System.out.println(cont);
+        }
+        System.out.println("Fim da contagem");
+    }
+}
