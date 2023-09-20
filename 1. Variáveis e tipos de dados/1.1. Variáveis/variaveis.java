a1a2a1c115748797afb6105f309b68774be3a785
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-20 16:29:41 -0300

    feat:Corrigido exemplos de variáveis

--- 1. Variáveis e tipos de dados/1.1. Variáveis/variaveis.java
+++ 1. Variáveis e tipos de dados/1.1. Variáveis/variaveis.java
@@ -1,8 +1,6 @@
-package ex1;
-
 public  class variaveis {
     public static void main(String[] args){
         int num=100;
-        System.out.printf("O valor da variável é num:%d",num);
+        System.out.printf("O valor da variável é :%d",num);
     }
 }
