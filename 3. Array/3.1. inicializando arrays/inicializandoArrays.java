3c7e0cfcfa3eb731bd2425ce3c5d0a1218e41add
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-20 17:12:03 -0300

    feat; Inicializan do array

---
+++ 3. Array/3.1. inicializando arrays/inicializandoArrays.java
@@ -0,0 +1,5 @@
+public class inicializandoArrays {
+    public static void main(String[] args) {
+        int[] num=new int[5];   //Declaração de arrays do tipo  int com 5 indices,porém é criado vazio 
+    }
+}
