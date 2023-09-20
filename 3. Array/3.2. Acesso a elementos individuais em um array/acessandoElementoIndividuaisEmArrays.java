dc62d03e81ca1be2cf722c6930f59eac1083b27d
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-20 17:21:30 -0300

    feat:Acessando arrays

---
+++ 3. Array/3.2. Acesso a elementos individuais em um array/acessandoElementoIndividuaisEmArrays.java
@@ -0,0 +1,6 @@
+public class acessandoElementoIndividuaisEmArrays {
+    public static void main(String[] args) {
+        int[] num=new int[5];
+        System.out.print(num[0]);//coram criados 5 indices e os indices são acessados por num[indice]
+    }
+}
