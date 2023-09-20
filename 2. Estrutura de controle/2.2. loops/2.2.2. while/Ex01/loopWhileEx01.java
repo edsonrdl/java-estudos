85406f086be25d4429125b9eaf0bbd9191867f51
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-20 16:29:57 -0300

    featCriado Exemplo da utilizando do loop while

---
+++ 2. Estrutura de controle/2.2. loops/2.2.2. while/Ex01/loopWhileEx01.java
@@ -0,0 +1,10 @@
+public class loopWhileEx01 {
+    public static void main(String[] args) {
+     int cont=0;
+        while(cont<=5){
+            System.out.println(cont +" "+ "esse o valor atual");
+            cont++;
+     }
+     System.out.println("Fim da execução");
+    }
+}
