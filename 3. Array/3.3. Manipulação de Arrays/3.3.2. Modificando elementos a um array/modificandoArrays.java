3acd11c86c2ef9a78dde7984322b2074fd516fed
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-21 00:35:23 -0300

    feat: Modificando array

---
+++ 3. Array/3.3. Manipulação de Arrays/3.3.2. Modificando elementos a um array/modificandoArrays.java
@@ -0,0 +1,9 @@
+public class modificandoArrays {
+    public static void main(String[] args) {
+        int[] arrayExemplo = new int[]{1, 2, 3, 4, 5}; // Outra forma de inicializar um array.
+        System.out.println("O valor atual do indice 0 é " + arrayExemplo[0]);
+        //Modificando o valor atual do indice 0
+        arrayExemplo[0]=33;
+        System.out.println("O Atual valor do indice 0 é " + arrayExemplo[0]);
+       } 
+}
