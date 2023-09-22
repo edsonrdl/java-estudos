4ba45962d20178aa904a343af85ff17af7b149a8
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-22 17:27:56 -0300

    feat:corrigido declaração de matrizes bidimencionais

--- 4. Matrizes/4.2. Matrizes Bidimensionais/4.2.1. Declaração de Matrizes/declaracaoDematrizes.java
+++ 4. Matrizes/4.2. Matrizes Bidimensionais/4.2.1. Declaração de Matrizes/declaracaoDematrizes.java
@@ -3,7 +3,5 @@ public class declaracaoDematrizes {
         final int linhas=3;
         final int colunas=4;
         int[][] arrayExemplo=new int[linhas][colunas];
-        System.out.printf("%d",arrayExemplo[0][0]);//imprimir 0 por não ter sido adicionado valor
-        
     }
 }
