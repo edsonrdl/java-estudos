5237d4afc71ee6832c7df09c8a7f2fd4e15b1d58
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-22 00:31:46 -0300

    feat:iterando array com enhanced for

---
+++ 3. Array/3.4. Iteração de arrays/3.4.2. Usando enhacend for(forEach)/arrayComEnhacendForEach.java
@@ -0,0 +1,11 @@
+public class arrayComEnhacendForEach {
+    public static void main(String[] args) {
+        // Declarando e inicializando um array de inteiros
+        int[] numeros = {1, 2, 3, 4, 5};
+
+        // Usando um loop enhanced for para iterar pelos elementos do array
+        for (int n : numeros) {
+            System.out.println(n);
+        }
+    }
+}
