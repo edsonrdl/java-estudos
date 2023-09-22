c0372dc82c8e54c5bf5b96932acf31d1245a8aa9
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-22 15:48:47 -0300

    feat:iterando vetor

---
+++ 4. Matrizes/4.1. Matrizes Unidimensionais (Vetores)/4.1.3. Iteração em vetores/iteracaoEmVetores.java
@@ -0,0 +1,9 @@
+public class iteracaoEmVetores {
+    public static void main(String[] args) {
+        int[] vetor=new int[]{3,4,5,6};
+        for( int v:vetor){
+  System.out.printf("%d \n",v);//Acessar o indice 3 e imprimir o valor 6
+        }
+      
+     }
+}
