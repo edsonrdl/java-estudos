20113ed79d95a8c9058d479b6b481d45eab83d59
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-22 00:18:16 -0300

    feat: Criado iteração de array com loop for

---
+++ 3. Array/3.4. Iteração de arrays/3.4.1. Usando loop for/usandoLoopForArray.java
@@ -0,0 +1,9 @@
+public class usandoLoopForArray {
+    public static void main(String[] args) {
+        int[] arrayExemplo = new int[]{1, 2, 3, 4, 5}; 
+        //Percorrendo array com for
+        for(int i=0;i<arrayExemplo.length;i++){
+             System.out.printf("Valores dos indices do array %d \n" ,arrayExemplo[i]);
+        }
+       } 
+}
