610f9662edce1c12e29b2b05365e6d861cce0b1a
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-22 15:46:41 -0300

    feat:Acessando valores do vetor

---
+++ 4. Matrizes/4.1. Matrizes Unidimensionais (Vetores)/4.1.2. Acesso a Elementos/acessoAElementos.java
@@ -0,0 +1,6 @@
+public class acessoAElementos {
+    public static void main(String[] args) {
+        int[] vetor=new int[]{3,4,5,6};
+        System.out.printf("%d",vetor[3]);//Acessar o indice 3 e imprimir o valor 6
+     }
+}
