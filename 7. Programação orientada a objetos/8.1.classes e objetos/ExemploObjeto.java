1735bce8c1a112784b13a527531de04de500525e
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-22 23:52:13 -0300

    feat:POO exemplo de class e objeto

---
+++ 7. Programação orientada a objetos/8.1.classes e objetos/ExemploObjeto.java
@@ -0,0 +1,13 @@
+public class ExemploObjeto {
+    public static void main(String[] args) {
+        // Criando um objeto da classe Pessoa
+        Pessoa pessoa1 = new Pessoa();
+
+        // Definindo os valores dos atributos
+        pessoa1.nome = "Monkey D. Luffy";
+        pessoa1.recompensa = "3 Bilhões de Berries";
+
+        // Chamando o método saudacao do objeto
+        pessoa1.saudacao();
+    }
+}
---
+++ 7. Programação orientada a objetos/8.1.classes e objetos/Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa {
+    // Atributos da classe (variáveis de instância)
+    String nome;
+    String recompensa;
+
+    // Métodos da classe
+    public void saudacao() {
+        System.out.println("Olá, meu nome é " + nome + " e minha recompensa é de " + recompensa );
+    }
+}
+
