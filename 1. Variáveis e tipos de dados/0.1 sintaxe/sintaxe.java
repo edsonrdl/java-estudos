f466116db4b5080aad05a6190c0962289cbf3d42
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2025-08-23 15:12:17 -0300

    feat: demostracao para o nando

--- 1. Variáveis e tipos de dados/0.1 sintaxe/sintaxe.java
+++ 1. Variáveis e tipos de dados/0.1 sintaxe/sintaxe.java
@@ -1,6 +1,7 @@
 
 public class sintaxe{
 public static void main(String[] args) {
-System.out.print("Olá,Sou Monkey D. Luffy");
+System.out.print("Olá,Sou Monkey D. Luffy \n");
+System.out.print("Nando");
 }
 }
